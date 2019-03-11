ea06b7f28e5bd61c64a6ce673cbf53b3e35dab88
dionis condurache <deniscondurache@gmail.com>
2019-03-11 21:58:14 +0200

    Add existing file

---
+++ index.js
@@ -0,0 +1,7 @@
+var elements = ["HTML", 11, "JavaScript", 2, "CSS", 23, 12];
+
+elements.push("-1");
+elements.unshift("1000");
+elements.push("Last array element");
+
+alert(elements); // HTML, 11, JavaScript, 2, CSS, 23, 12;
